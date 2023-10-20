44d5c4d43cbfe707f4d782328e6ab972d464fce7
Traill Lyall <holycrapeh@hotmail.com>
2023-10-20 12:12:40 -0700

    added profile and artists pages

---
+++ public/js/artist_profile.js
@@ -0,0 +1,73 @@
+import Artist from './Artist.js';
+
+// get the artist id from the url with key "id"
+let artistId = new URLSearchParams(window.location.search).get("id");
+let artist = Artist.artists[artistId];
+let selectedImageIndex = 0;
+
+document.getElementById("artistName").innerHTML += artist.name;
+document.getElementById("artistBio").innerHTML += artist.bio;
+document.getElementById("artistEmail").innerHTML += artist.email;
+document.getElementById("artistPhone").innerHTML += artist.phone;
+document.getElementById("artistWebsite").href = artist.website;
+document.getElementById("artistWebsite").innerHTML = artist.website;
+document.getElementById("facebook").href = artist.facebook;
+document.getElementById("instagram").href = artist.instagram;
+
+let imageContainer = document.getElementById("artistImagesContainer");
+let dialog = document.getElementById("imageDialog");
+let dialogImage = dialog.getElementsByTagName("img")[0];
+
+let previousButton = document.getElementById("previousImage");
+let nextButton = document.getElementById("nextImage");
+
+previousButton.onclick = function()
+{
+    selectedImageIndex = (selectedImageIndex - 1) % artist.images.length;
+    if (selectedImageIndex < 0)
+    {
+        selectedImageIndex = artist.images.length - 1;
+    }
+    dialogImage.src = artist.images[selectedImageIndex];
+}
+
+nextButton.onclick = function()
+{
+    selectedImageIndex = (selectedImageIndex + 1) % artist.images.length;
+    dialogImage.src = artist.images[selectedImageIndex];
+    
+}
+
+// populate the image container with images
+for (let i = 0; i < artist.images.length; i++)
+{
+    let imageFigure = document.createElement("figure");
+    
+    let image = document.createElement("img");
+    image.src = artist.images[i];
+
+    //let imageCaption = document.createElement("figcaption");
+    //imageCaption.innerHTML = artist.categories[i];
+
+    imageFigure.appendChild(image);
+    //imageFigure.appendChild(imageCaption);
+
+    imageContainer.appendChild(imageFigure);
+
+    //create a dialog box with an image and a caption when the image is clicked
+    image.onclick = function()
+    {
+        dialogImage.src = image.src;
+        selectedImageIndex = i;
+        dialog.showModal();
+    }
+
+    
+}
+
+function closeDialog()
+{
+    dialog.close();
+}
+
+window.closeDialog = closeDialog;
---
+++ public/js/index.js
@@ -0,0 +1,129 @@
+import Artist from './Artist.js';
+let artistsContainer = document.getElementById("artistsContainer");
+let genreFieldset = document.getElementById("genre");
+let mediumFieldset = document.getElementById("medium");
+let culturalFieldset = document.getElementById("cultural");
+
+let checkboxes = [];
+let artists = Artist.artists;
+
+for (let i = 0; i < Artist.genreCategories.length; i++)
+{
+    let category = Artist.genreCategories[i];
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = category;
+    checkbox.value = category;
+    checkbox.checked = false;
+    checkbox.onclick = generateArtists;
+    checkbox.innerHTML = category;
+
+    let label = document.createElement("label");
+    label.htmlFor = category;
+    label.innerHTML = category;
+
+    let br = document.createElement("br");
+
+    genreFieldset.appendChild(checkbox);
+    genreFieldset.appendChild(label);
+    genreFieldset.appendChild(br);
+    checkboxes.push(checkbox);
+}
+
+for (let i = 0; i < Artist.mediumCategories.length; i++)
+{
+    let category = Artist.mediumCategories[i];
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = category;
+    checkbox.value = category;
+    checkbox.checked = false;
+    checkbox.onclick = generateArtists;
+
+    let label = document.createElement("label");
+    label.htmlFor = category;
+    label.innerHTML = category;
+
+    let br = document.createElement("br");
+
+    mediumFieldset.appendChild(checkbox);
+    mediumFieldset.appendChild(label);
+    mediumFieldset.appendChild(br);
+    checkboxes.push(checkbox);
+}
+
+for (let i = 0; i < Artist.culturalCategories.length; i++)
+{
+    let category = Artist.culturalCategories[i];
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = category;
+    checkbox.value = category;
+    checkbox.checked = false;
+    checkbox.onclick = generateArtists;
+
+    let label = document.createElement("label");
+    label.htmlFor = category;
+    label.innerHTML = category;
+
+    let br = document.createElement("br");
+
+    culturalFieldset.appendChild(checkbox);
+    culturalFieldset.appendChild(label);
+    culturalFieldset.appendChild(br);
+    checkboxes.push(checkbox); 
+}
+
+generateArtists();
+
+function generateArtists()
+{
+    artistsContainer.innerHTML = "";
+
+    let selectedCategories = [];
+
+    for (let i = 0; i < checkboxes.length; i++)
+    {
+        let checkbox = checkboxes[i];
+
+        if (checkbox.checked)
+        {
+            selectedCategories.push(checkbox.value);
+        }
+    }
+
+    for (let i = 0; i < artists.length; i++)
+    {
+        let artist = artists[i];
+
+        if(selectedCategories.length > 0 && !selectedCategories.some(category => artist.categories.includes(category)))
+        {
+            continue;
+        }
+
+        let artistFigure = document.createElement("figure");
+        artistFigure.classList.add("artist");
+
+        let artistImage = document.createElement("img");
+
+        artistImage.src = artist.images[0];
+
+        let artistCaption = document.createElement("figcaption");
+        artistCaption.innerHTML = artist.name;
+
+        artistFigure.appendChild(artistImage);
+        artistFigure.appendChild(artistCaption);
+
+        artistFigure.onclick = function()
+        {
+            window.location.href = "artist_profile.html?id=" + i;
+        }
+
+        artistsContainer.appendChild(artistFigure);
+    }
+}
+
+
