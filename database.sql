d1aa92b57df98aa30bbab5043aeabe351fbd37ae
JamesLin2022 <james.09461@gmail.com>
2023-11-06 22:50:59 -0800

    updating sql

--- database.sql
+++ database.sql
@@ -1,59 +1,84 @@
-create database if not exists roster_database;
+create database if not exists rosterDatabase;
 
-use roster_database;
+use rosterDatabase;
 
 -- general user data
 create table if not exists user (
-    user_id int not null auto_increment,
+    userID int not null auto_increment,
     email varchar(31) not null,
     password varchar(31),
     admin bit default 0,
     verified bit default 0,
-    creation_date datetime default current_timestamp,
-    verified_date datetime,
-    primary key (user_id, email)
+    creationDate datetime default current_timestamp,
+    verifiedDate datetime,
+    primary key (userID, email)
+);
+
+-- user application
+create table if not exists userApplication (
+    userID int not null,
+    applicationID varchar(15) not null unique,
+    name varchar(31) not null,
+    email varchar(31) not null,
+    phone varchar(15),
+    website varchar(127),
+    instagramHandle varchar(127),
+    facebookHandle varchar(127),
+    bcResident bit,
+    experience bit,
+    experienceDescription varchar(511),
+    biography varchar(1023),
+    genre varchar(1023),
+    cultural varchar(1023),
+    preference varchar(1023),
+    approved bit default 0,
+    rejectionReason varchar(255),
+    primary key (userID, applicationID),
+    foreign key (userID) references user(userID) on delete cascade	
 );
 
 -- user profile
-create table if not exists user_profile (
-    user_id int not null,
+create table if not exists userProfile (
+    userID int not null,
     name varchar(31) not null,
     phone varchar(15),
     biography varchar(1023),
-    primary key (user_id),
-    foreign key (user_id) references user(user_id) on delete cascade	
+    primary key (userID),
+    foreign key (userID) references user(userID) on delete cascade	
 );
 
 -- user social media
-create table if not exists user_media (
-    user_id int not null,
+create table if not exists userMedia (
+    userID int not null,
     website varchar(127),
     facebook varchar(127),
     instagram varchar(127),
-    x varchar(127), 
+    twitter varchar(127), 
     linkedin varchar(127),
     youtube varchar(127),
-    primary key (user_id),
-    foreign key (user_id) references user(user_id) on delete cascade
+    primary key (userID),
+    foreign key (userID) references user(userID) on delete cascade
 );
 
 -- user image
-create table if not exists user_art (
-    user_id int not null,
-    image_id varchar(63) not null unique,
+create table if not exists userArt (
+    userID int not null,
+    imageID varchar(15) not null unique,
     name varchar(31),
-    description varchar(511),
+    description varchar(255),
     verified bit default 0,
-    upload_date datetime default current_timestamp,
-    verified_date datetime,
-    primary key (user_id, image_id),
-    foreign key (user_id) references user(user_id) on delete cascade
+    uploadDate datetime default current_timestamp,
+    verifiedDate datetime,
+    primary key (userID, imageID),
+    foreign key (userID) references user(userID) on delete cascade
 );
 
 -- user image category
-create table if not exists user_art_category (
-    user_id int not null,
-    image_id varchar(15) not null,
-    category_one bit,
-    primary key (user_id, image_id)
+create table if not exists userArtCategory (
+	userID int not null,
+    imageID varchar(15) not null,
+    categoryOne bit,
+    primary key (userID, imageID),
+    foreign key (userID) references user(userID) on delete cascade,
+    foreign key (imageID) references userArt(imageID) on delete cascade
 );
