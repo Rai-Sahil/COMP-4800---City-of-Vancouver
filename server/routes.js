ef4a66a776def879214c9f1d27fab46cff1437b0
traillio <tlyall2@my.bcit.ca>
2023-11-13 17:09:43 -0800

    fixed some issues

--- server/routes.js
+++ server/routes.js
@@ -86,8 +86,8 @@ app.post('/userform-submit', (req, res) => {
 
     // Switch to the image upload page
     // REPLACE user.name with the artistId
-    res.render('Components/imageform', { artistId: user.name });
-
+    //res.render('Components/imageform', { artistId: user.name });
+    res.render('Components/successfullSubmission');
 });
 
 app.use("/admin", (req, res, next) => {
@@ -175,24 +175,30 @@ function generateAdminDashboard() {
     return dashboard;
 }
 
-app.post('/imageUpload', (req, res) => {
-    upload()(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
+app.post('/imageUpload', (req, res) => 
+{
+
+    upload()(req, res, function (err) 
+    {
+        if (err instanceof multer.MulterError) 
+        {
             return res.status(400).json("Multer error");
-        }
-        else if (err) {
+        } 
+        else if (err) 
+        {
             return res.status(400).json("Unknown error");
         }
 
-        bob(req, res);
+        createFiles(req, res);
     });
 
-    const bob = async (req, res) => {
-        //Chnage this to artist ID
-        let artistId = req.body.name;
+    const createFiles = async (req, res) =>
+    {
+        let artistId = req.body.artistId;
 
         const regex = /^[a-zA-Z0-9]{1,20}$/;
-        if (!regex.test(artistId)) {
+        if(!regex.test(artistId))
+        {
             res.status(400).send("Invalid artistId");
             return;
         }
@@ -201,22 +207,39 @@ app.post('/imageUpload', (req, res) => {
         // TODO
 
         const path = `public/artistImages/${artistId}/`;
-        if (fs.existsSync(path)) {
-            fs.rmSync(path, { recursive: true });
+        if(fs.existsSync(path))
+        {
+            // check if artist has been approved
+            // TODO
+
+            
+            // if artist has been approved, dont let them upload again
+            // {
+            //     res.status(400).send("Artist has already been approved, cannot upload again");
+            //     return;
+            // }
+            // else
+            // {
+                fs.rmSync(path, { recursive: true});
+            // }
         }
 
         fs.mkdirSync(path, { recursive: true });
         let files = req.files;
 
-        try {
-            for (let i = 0; i < files.length; i++) {
+        try
+        {
+            for (let i = 0; i < files.length; i++)
+            {
                 await sharp(files[i].buffer).toFormat('jpeg').toFile(path + i + '.jpeg');
             }
         }
-        catch (err) {
+        catch (err)
+        {
             //delete all files we just uploaded
-            if (fs.existsSync(path)) {
-                fs.rm(path, { recursive: true });
+            if(fs.existsSync(path))
+            {
+                fs.rm(path, { recursive: true});
             }
             res.status(400).send("Error converting images");
         }
@@ -226,24 +249,68 @@ app.post('/imageUpload', (req, res) => {
 }
 );
 
-const upload = (artistId) => {
-    let currentFile = 0;
+const upload = (artistId) => 
+{
+  return imageUpload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: FILESIZE_MAX_BYTES },
+    fileFilter: function (req, file, cb) 
+    {
+      var filetypes = /jpeg|jpg|png/;
+      var mimetype = filetypes.test(file.mimetype);
+      var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-    return imageUpload = multer({
-        storage: multer.memoryStorage(),
-        limits: { fileSize: FILESIZE_MAX_BYTES },
-        fileFilter: function (req, file, cb) {
-            var filetypes = /jpeg|jpg|png/;
-            var mimetype = filetypes.test(file.mimetype);
-            var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+      if (mimetype && extname) 
+      {
+        return cb(null, true);
+      }
 
-            if (mimetype && extname) {
-                return cb(null, true);
-            }
-
-            cb("Error: File upload only supports the following filetypes - " + filetypes);
-        }
-    }).array('image', 8);
+      cb("Error: File upload only supports the following filetypes - " + filetypes);
+    }
+  }).array('image', 8);
 }
+
+
+app.delete("/imageUpload", (req, res) => 
+{
+    const artistId = req.body.artistId;
+    const token = req.body.token;
+
+    if (token !== secretToken)
+    {
+        res.status(403).send("Access Denied");
+        return;
+    }
+
+    const regex = /^[a-zA-Z0-9]{1,20}$/;
+    if (!regex.test(artistId)) {
+        res.status(400).send("Invalid artistId");
+        return;
+    }
+
+    // check if artistId exists
+    // TODO
+    
+    const path = `public/artistImages/${artistId}/`;
+    if (fs.existsSync(path)) 
+    {
+        try
+        {
+            fs.rmSync(path, { recursive: true });
+        }
+        catch (err)
+        {
+            res.status(400).send("Error deleting images");
+            return;
+        }
+    }
+    else 
+    {
+        res.status(400).send("Could not delete, directory does not exist");
+        return;
+    }
+
+    res.send("Success");
+});
 
 module.exports = app;
