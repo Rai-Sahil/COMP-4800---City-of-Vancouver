faf9d1ba11e15b4c7e2365a72cdb5b914084cc01
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-11-20 01:32:12 -0800

    added firebase prototype

--- public/js/site.js
+++ public/js/site.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch("header")
+    fetch("header.html")
         .then((response) => response.text())
         .then((data) => {
             document.getElementById("header").innerHTML = data;
