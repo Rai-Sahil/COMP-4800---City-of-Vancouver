6ff8a6982dd795567e5fce019ff4638705860abe
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-11-20 15:36:10 -0800

    fixed userforms to show an error if fields are not filled out or selected.

--- public/js/userform.js
+++ public/js/userform.js
@@ -1,25 +1,8 @@
 const biographyLetterCountMaximum = 1000;
 const experienceLetterCountMaximum = 500;
 const errorMaximumLetterCount = 'Your text exceed the maximum character limit of ';
-
-
-// Function to toggle the visibility of the experience description textarea and its label
-function toggleExperienceDescription() {
-  var descriptionTextArea = document.getElementById("experienceDescription");
-  var descriptionLabel = document.getElementById("experienceDescriptionLabel");
-  var yesExperienceRadio = document.getElementById("yesExperience");
-
-  // Show the textarea and its label if "Yes" is selected, hide them otherwise
-  if (yesExperienceRadio.checked) {
-    descriptionTextArea.style.display = "block";
-    descriptionLabel.style.display = "block";
-    descriptionTextArea.required = true;
-  } else {
-    descriptionTextArea.style.display = "none";
-    descriptionLabel.style.display = "none";
-    descriptionTextArea.required = false;
-  }
-}
+const errorNoGenreSelected = 'Please select at least one genre.';
+const errorNoMediumSelected = 'Please select at least one medium.';
 
 // Function that limits the number of letters in a textarea
 function limitLetters(e, limit, counterElementId) {
@@ -28,15 +11,27 @@ function limitLetters(e, limit, counterElementId) {
   document.getElementById(counterElementId).textContent = letters + '/' + limit;
 
   if (letters > limit) {
-      document.getElementById(counterElementId).textContent = errorMaximumLetterCount + limit;
-      document.getElementById(counterElementId).style.color = 'red';
-      e.target.value = e.target.value.slice(0, limit);
+    document.getElementById(counterElementId).textContent = errorMaximumLetterCount + limit;
+    document.getElementById(counterElementId).style.color = 'red';
+    e.target.value = e.target.value.slice(0, limit);
   } else {
-      document.getElementById(counterElementId).style.color = 'black';
+    document.getElementById(counterElementId).style.color = 'black';
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  var yesExperienceRadio = document.getElementById('yesExperience');
+  var noExperienceRadio = document.getElementById('noExperience');
+  var experienceDescription = document.getElementById('experienceDescription');
+
+  yesExperienceRadio.addEventListener('change', function () {
+    experienceDescription.required = this.checked;
+  });
+
+  noExperienceRadio.addEventListener('change', function () {
+    experienceDescription.required = !this.checked;
+  });
+
   document.getElementById('biography').addEventListener('input', function (e) {
     limitLetters(e, biographyLetterCountMaximum, 'letterCount');
   });
@@ -44,4 +39,19 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('experienceDescription').addEventListener('input', function (e) {
     limitLetters(e, experienceLetterCountMaximum, 'experienceLetterCount');
   });
+
+
+  document.getElementById('contactForm').addEventListener('submit', function (e) {
+    var genreCheckboxes = document.querySelectorAll('input[name="genre"]:checked');
+    var mediumCheckboxes = document.querySelectorAll('input[name="medium"]:checked');
+
+    if (genreCheckboxes.length === 0) {
+      alert(errorNoGenreSelected);
+      e.preventDefault(); // Prevent the form from submitting
+    } else if (mediumCheckboxes.length === 0) {
+      alert(errorNoMediumSelected);
+      e.preventDefault();
+    }
+  });
 });
+
