33085545d363cf5846654e94425d995af8da0e99
Traillio <tlyall2@my.bcit.ca>
2023-12-02 17:09:37 -0800

    added dates to user_application and fixed code someone deleted in routes.js ) =<

--- server/routes.js
+++ server/routes.js
@@ -235,18 +235,41 @@ app.get('/artists', (req, res) => {
 
 });
 
-app.get('/artists/single', (req, res) => {
+app.get('/artists/single', (req, res) => 
+{
     const artistId = req.query.id;
 
     const query = `CALL getArtistById(${artistId});`;
 
-    mainConnection.query(query, function (err, result) {
-        if (err) {
+    mainConnection.query(query, function(err, result)
+    {
+        if (err)
+        {
 
             res.status(500).send("Could not get artists");
             return;
             //throw err;  
         }
+
+        if(result[0].length == 0)
+        {
+            res.status(404).send("Artist not found");
+            return;
+        }
+
+        let artist = result[0][0];
+
+        artist.images = [];
+        // SWITCH TO UUID
+        let imagePaths = fs.readdirSync(`public/artistImages/${artistId}/`, { withFileTypes: true });
+        for (let j = 0; j < imagePaths.length; j++)
+        {
+            artist.images.push(`artistImages/${artistId}/${imagePaths[j].name}`);
+        }
+            
+
+        res.contentType('application/json');
+        res.json(artist);
     });
 });
 
